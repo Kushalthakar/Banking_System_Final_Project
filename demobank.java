008605f884d631045fd95ab191bc89c9a54613a0
Kushal Thakar <36340257+Kushalthakar@users.noreply.github.com>
2021-03-25 23:11:39 +0530

    Update demobank.java

--- demobank.java
+++ demobank.java
@@ -1,3 +1,5 @@
+//import CurrentPackage.*;
+//import  SavingsPackage.*;
 import java.util.*;
 import java.text.*;
 import java.io.*;
@@ -7,25 +9,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.lang.*;
-public class demobank 
+
+public class demobank
 {
-    
+
     public static void main(String[] args) throws IOException
-     {
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Press 1 for Current account");
         System.out.println("Press 2 for Savings Account");
         System.out.println("Enter choice:");
         int c = sc.nextInt();
         switch(c)
-        {   
+        {
             case 1:
                 current.democurrent();
             case 2:
                 savings.demosavings();
-                
-
-                
         }
-     }
+    }
 }
